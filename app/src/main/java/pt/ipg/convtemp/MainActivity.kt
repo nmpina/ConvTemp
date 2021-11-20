8fb9e838d7850880daf37f482574a880168a269d
nelson <nmpina28@gmail.com>
2021-11-20 11:27:35 +0000

    Main Activity view refactor. Add Conversion function to main activity

--- app/src/main/java/pt/ipg/convtemp/MainActivity.kt
+++ app/src/main/java/pt/ipg/convtemp/MainActivity.kt
@@ -3,7 +3,6 @@ package pt.ipg.convtemp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
 import android.widget.RadioButton
 
@@ -16,7 +15,23 @@ class MainActivity : AppCompatActivity() {
     fun converte(view: android.view.View) {
         val editTemperatura = findViewById<EditText>(R.id.editTemperatura)
         val radioButtonFahrenheit = findViewById<RadioButton>(R.id.radioButtonFahrenheit)
-        val  radioButtonCelsius = findViewById<RadioButton>(R.id.radioButtonCelsius)
+        val radioButtonCelsius = findViewById<RadioButton>(R.id.radioButtonCelsius)
+
+        val valorTemperatura = editTemperatura.toString().toDoubleOrNull()
+        if(valorTemperatura == null) {
+            editTemperatura.error = getString(R.string.temperatura_invalida)
+            editTemperatura.requestFocus()
+            return
+        }
+
+        if (radioButtonFahrenheit.isChecked) {
+            DadosApp.temperatura = TemperaturaFahrenheit(valorTemperatura)
+        } else if (radioButtonCelsius.isChecked) {
+            DadosApp.temperatura = TemperaturaCelsius(valorTemperatura)
+        } else {
+            radioButtonFahrenheit.error = "Escolha a unidade de temperatura"
+            return
+        }
 
         val intent = Intent(this, VerConversaoActivity::class.java)
         startActivity(intent)
